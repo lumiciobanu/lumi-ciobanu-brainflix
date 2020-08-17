343abdd74376c34988f5d125c686c06c9fb02f11
Lumi Ciobanu <lumiciobanu@gmail.com>
2020-08-18 00:10:25 +0300

    Monday night - folder structure

---
+++ sprint-3/client/build/precache-manifest.7e6f95fb0565faebacd502be9dc7286b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cf7b87a54f8ff194ffb744c99f9f97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aeafd30b318cff34a78",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "288be8690319eed668d3",
+    "url": "/static/js/2.0afd874a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0afd874a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aeafd30b318cff34a78",
+    "url": "/static/js/main.a3347a47.chunk.js"
+  },
+  {
+    "revision": "04f9668f1fbe50a62cd8",
+    "url": "/static/js/runtime-main.ff702af5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ sprint-3/server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express(); 
+
+app.get('/', (req, res) => {
+    // send some text back as a response
+    res.send('Express is running!');
+});
+
+
+// start Express on port 8080
+app.listen(8080, () => {
+    console.log('Hi Started on http://localhost:8080');
+    console.log('Press CTRL + C to stop server');
+});
+
